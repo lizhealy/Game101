e971288a7cea79ae8335d9412d9d4f85ccffe233
husnasidd <hsiddiqu@citadel.edu>
2015-10-27 01:57:58 +0000

    view no longer hard coded

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,18 +1,20 @@
 class QuestionsController < ApplicationController
-    
-    
     # def show
     #     id = params[:category] # retrieve movie ID from URI route
     #     @question = Question.find(id) # look up movie by unique ID
     #     # will render app/views/movies/show.<extension> by default
     # end
 
-    
     def show
         category_choice = params[:category]
-        # @questions = Question.all
+        @questions = Question.all
         redirect_to questions_path(category_choice)
+
     end
+    
+  def index
+    @all_categories = Question.uniq.pluck(:category)
+  end
     
     def get_all_categories
         @all_categories = Question.uniq.pluck(:category)
